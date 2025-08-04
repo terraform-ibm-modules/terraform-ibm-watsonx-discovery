420363ee0684fc69d2b9ee42b9543f7048173efd
prateek <prateek.sharma13@ibm.com>
2025-08-04 22:11:21 +0530

    feat: enhance existing DA for improved user experience <br>- update catalog config<br>- update variable descriptions<br>-modify IAM permissions for DA<br>- update DA readme(#94)
    * fix: ux da issues
    
    * refactor: minor changes and observability split
    
    * refactor: minor changes
    
    * refactor: reverted split observability and other suggestions included
    
    * fixed typo
    
    * updated support note
    
    * updated support note
    
    * added icon
    
    * added at in permissions and updated kms notes

--- solutions/fully-configurable/variables.tf
+++ solutions/fully-configurable/variables.tf
@@ -9,7 +9,7 @@ variable "ibmcloud_api_key" {
 }
 
 variable "provider_visibility" {
-  description = "Set the visibility value for the IBM terraform provider. Supported values are `public`, `private`, `public-and-private`. [Learn more](https://registry.terraform.io/providers/IBM-Cloud/ibm/latest/docs/guides/custom-service-endpoints)."
+  description = "Set the visibility value for the IBM terraform provider. [Learn more](https://registry.terraform.io/providers/IBM-Cloud/ibm/latest/docs/guides/custom-service-endpoints)."
   type        = string
   default     = "private"
 
@@ -21,7 +21,7 @@ variable "provider_visibility" {
 
 variable "existing_resource_group_name" {
   type        = string
-  description = "The name of an existing resource group to provision the Watson.discovery in."
+  description = "The name of an existing resource group to provision the Watson Discovery instance."
   default     = "Default"
 }
 
@@ -31,6 +31,11 @@ variable "prefix" {
   description = "The prefix to be added to all resources created by this solution. To skip using a prefix, set this value to null or an empty string. The prefix must begin with a lowercase letter and may contain only lowercase letters, digits, and hyphens '-'. It should not exceed 16 characters, must not end with a hyphen('-'), and can not contain consecutive hyphens ('--'). Example: wx-0205-di.[Learn more](https://terraform-ibm-modules.github.io/documentation/#/prefix.md)"
 
   validation {
+    # - null and empty string is allowed
+    # - Must not contain consecutive hyphens (--): length(regexall("--", var.prefix)) == 0
+    # - Starts with a lowercase letter: [a-z]
+    # - Contains only lowercase letters (a–z), digits (0–9), and hyphens (-)
+    # - Must not end with a hyphen (-): [a-z0-9]
     condition = var.prefix == null || var.prefix == "" ? true : alltrue([
       can(regex("^[a-z][-a-z0-9]*[a-z0-9]$", var.prefix)), length(regexall("--", var.prefix)) == 0
     ])
@@ -38,6 +43,7 @@ variable "prefix" {
   }
 
   validation {
+    # must not exceed 16 characters in length
     condition     = var.prefix == null || var.prefix == "" ? true : length(var.prefix) <= 16
     error_message = "Prefix must not exceed 16 characters."
   }
@@ -50,14 +56,14 @@ variable "watson_discovery_instance_name" {
 }
 
 variable "region" {
-  description = "The region where you want to deploy your instance."
+  description = "The region to provision all resources in. [Learn more](https://terraform-ibm-modules.github.io/documentation/#/region) about how to select different regions for different services."
   type        = string
   default     = "us-south"
 }
 
 variable "service_plan" {
   type        = string
-  description = "The plan that is required to provision the Watson Discovery instance. Possible values are: plus, enterprise. [Learn more](https://cloud.ibm.com/docs/discovery-data?topic=discovery-data-pricing-plans)."
+  description = "The plan that is required to provision the Watson Discovery instance. [Learn more](https://cloud.ibm.com/docs/discovery-data?topic=discovery-data-pricing-plans)."
   default     = "plus"
 }
 
@@ -74,7 +80,7 @@ variable "access_tags" {
 }
 
 variable "service_endpoints" {
-  description = "Types of the service endpoints that can be applied to a Watson Discovery instance. Possible values are : public, private or public-and-private."
+  description = "Types of the service endpoints that can be applied to a Watson Discovery instance."
   type        = string
-  default     = "private"
+  default     = "public-and-private"
 }

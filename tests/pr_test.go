32e18aeaf8de8e61433cf14b0378cf500a01bc99
whoffler <WHoffler@ie.ibm.com>
2025-10-21 09:32:28 +0100

    update dependencies and prefix input, remove account infra dep

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -216,7 +216,6 @@ func TestDefaultConfiguration(t *testing.T) {
 		"deploy-arch-ibm-watson-discovery",
 		"fully-configurable",
 		map[string]interface{}{
-			"prefix":                       options.Prefix,
 			"existing_resource_group_name": resourceGroup,
 		},
 	)
